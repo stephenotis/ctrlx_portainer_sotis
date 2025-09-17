4cdb925b0a088973497e8f5a03b3f0691e8c2b9c
stephenotis <sotis85@gmail.com>
2025-09-17 08:18:44 -0700

    Update build_snap.sh
    Move snap builds commands to correct script

--- build_snap.sh
+++ build_snap.sh
@@ -3,10 +3,13 @@ TARGET_ARCH=$(dpkg --print-architecture)
 if [[ -n $1 ]]; then
    TARGET_ARCH=$1
 fi
+set -e
 echo TARGET_ARCH: ${TARGET_ARCH}
-
 echo --- clean snap
-snapcraft clean --destructive-mode
+sudo snapcraft clean
 
 echo --- build snap with architecture ${TARGET_ARCH}
-snapcraft --destructive-mode --enable-experimental-target-arch --target-arch=${TARGET_ARCH}
+sudo snapcraft pack --build-for=${TARGET_ARCH} --verbosity=verbose
+
+echo --- clean snap
+sudo snapcraft clean 
