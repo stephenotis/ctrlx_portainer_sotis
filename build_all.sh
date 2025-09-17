113b82d7bae1047603b49cd4c95db3c95875e4d9
stephenotis <sotis85@gmail.com>
2025-09-17 08:20:04 -0700

    Update build_all.sh
    Fix master script

--- build_all.sh
+++ build_all.sh
@@ -3,13 +3,10 @@ TARGET_ARCH=$(dpkg --print-architecture)
 if [[ -n $1 ]]; then
    TARGET_ARCH=$1
 fi
-set -e
 echo TARGET_ARCH: ${TARGET_ARCH}
-echo --- clean snap
-sudo snapcraft clean
 
-echo --- build snap with architecture ${TARGET_ARCH}
-sudo snapcraft pack --build-for=${TARGET_ARCH} --verbosity=verbose
+echo --- build content
+bash build_content.sh ${TARGET_ARCH}
 
-echo --- clean snap
-sudo snapcraft clean 
+echo --- build snap
+bash build_snap.sh ${TARGET_ARCH}
